24271a0c21939d0593ee5dfaabeb225bc60d6e1e
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2024-05-17 15:20:38 +0200

    fix: test e2e

--- frontend/src/components/dashboard/DashboardLayout.tsx
+++ frontend/src/components/dashboard/DashboardLayout.tsx
@@ -1,6 +1,8 @@
+import Typography from '../commons/typography/Typography';
+import ListBooks from '../example/Book';
+import Title from '../example/Title';
 import LastActivitiesListWidget from './lastActivitiesList/LastActivitiesListWidget';
 import LastPostWidget from './lastPost/LastPostWidget';
-import Typography from '@/components/commons/typography/Typography';
 
 export default function DashboardLayout() {
   return (
@@ -8,12 +10,16 @@ export default function DashboardLayout() {
       <div className='w-7/12 h-full'>
         <div className='h-[44%]  p-3'>
           <div className='dashboardWidget h-full'>
-            <Typography variant='heading'>Dépenses du mois</Typography>
+            <Typography variant='heading'>Mes dépenses du mois</Typography>
+            <h1>Bienvenuey</h1>
+            Voici un résumé de vos dépenses mensuelles
+            <ListBooks />
+            <Title />
           </div>
         </div>
         <div className='h-[28%]  p-3'>
           <div className='dashboardWidget h-full'>
-            <Typography variant='heading'>Dépenses annuelles</Typography>
+            <Typography variant='heading'>Dépenses anuelles</Typography>
           </div>
         </div>
         <div className='h-[28%]  p-3'>
