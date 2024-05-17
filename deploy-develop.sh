0b19ddfa5fed4a7c0784785a3b3cb69edc6191e1
Matthias <matthias.vimbert@gmail.com>
2024-05-17 14:03:05 +0200

    fix: minor fix on script deploy

--- deploy-develop.sh
+++ deploy-develop.sh
@@ -2,4 +2,3 @@
 git fetch origin && git reset --hard origin/develop && git clean -f -d && \
 docker compose -f docker-compose.develop.yml down && \
 docker compose -f docker-compose.develop.yml pull && \
-set GATEWAY_PORT=8124 docker compose -f docker-compose.develop.yml up -d;
