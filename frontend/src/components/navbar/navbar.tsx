a46c3e559eaa13acc1ed1d3c0afec7e2af883bc8
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2024-05-17 12:25:29 +0200

    style: home layout margin updated

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -33,7 +33,7 @@ export default function navbar() {
   return (
     <>
       <div className='flex items-center justify-center h-screen ml-5 mr-5'>
-        <div className='flex flex-col h-[95vh] bg-black text-white w-36 rounded-3xl'>
+        <div className='flex flex-col h-[98vh] bg-black text-white w-36 rounded-3xl'>
           <div className='logo flex items-center justify-center relative mt-5'>
             <Link href='/'>
               <svg
