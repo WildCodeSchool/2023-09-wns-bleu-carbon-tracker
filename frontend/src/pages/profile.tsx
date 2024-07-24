af606a1880451bfb086349e54d0aa957009f149b
Eric_Gdf <ericgodefroy@outlook.fr>
2024-07-24 15:51:43 +0200

    upload image service

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -12,6 +12,7 @@ import {
 } from '@/graphql/generated/schema';
 import Button from '@/components/commons/buttons/Button';
 import GenericFormModal from '@/components/modal/GenericFormModal';
+import uploadImage from '@/uploadImage';
 
 export default function Profile() {
   const { user, setUser } = useUser();
@@ -36,13 +37,9 @@ export default function Profile() {
     if (file) {
       form.append('file', file);
       try {
-        const res = await axios.post('http://localhost:8000/uploads', form, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-
-        const imageUrl: string = res.data.url;
+        const res = await uploadImage(file);
+        let imageUrl = '';
+        if (res) imageUrl = res.data.url;
         const updatedUser = await updateUser({
           variables: {
             picture: imageUrl,
